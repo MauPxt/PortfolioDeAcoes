0599ddfa9a8d1c66ad691d44404b84d9ffe92e00
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-21 21:13:40 -0300

    refactor: upgrade function and efficience of code

--- fronteira_eficiencia.R
+++ fronteira_eficiencia.R
@@ -2,43 +2,44 @@ require(quantmod)
 require(PerformanceAnalytics)
 library(fPortfolio)
 
-# criando uma função para calcular os retornos. A função consegue receber uma quantidade indefinida de papéis
-calcularRetornos <- function(...) {
-  # Pegar todos os simbolos passados para a função
-  symbols <- c(...)
-
-  # Baixear os dados e calcular o retorno de cada um dos papéis
-  listaRetornos <- purrr::map(symbols, function(symbol) {
-    p <- getSymbols(symbol, src = "yahoo", auto.assign = FALSE)
-    p <- Cl(p)
-    r <- Return.calculate(p)
-    rb <- apply.weekly(r, mean)
-    return(rb)
+# Função para calcular os retornos semanais de uma lista de ativos
+calcularRetornos <- function(symbols, de='2020-01-01', ate='2020-12-31') {
+  retornos <- lapply(symbols, function(symbol) {
+    data <- getSymbols(symbol, src = "yahoo", auto.assign = FALSE, from = de, to = ate)
+    p <- data[, paste0(symbol, ".Close")]
+    r <- Return.calculate(na.omit(p))
+    apply.weekly(r, mean)
   })
 
-  # Convertendo o resultado em uma série temporal
-  retornos <- as.timeSeries(listaRetornos)
-  
-  return(retornos)
+  retornos <- na.omit(do.call(cbind, retornos))
+  as.timeSeries(retornos)
 }
 
-# Chamando a função com os papéis
-retornos <- calculate_returns("BOVA11.SA","BPAN4.SA")
+# Lista de ativos
+acoes <- c("BOVA11.SA", "SANB11.SA", "BBAS3.SA", "BBDC4.SA", "ITUB4.SA")
 
-# Montando um portfólio com foco na eficiencia
+# Calculando os retornos semanais
+retornos <- calcularRetornos(acoes, de='2021-01-01', ate='2022-12-31')
+
+# Construindo o portfólio que tangencia a fronteira de eficiência
 portfolio_eficiente <-
   tangencyPortfolio(retornos, spec = portfolioSpec(), constraints = "LongOnly")
 portfolio_eficiente
 
-# Montando um portfólio com foco na minimização dos riscos
+# Construindo o portfólio de menor risco
 portfolio_menor_risco <-
   minvariancePortfolio(retornos, spec = portfolioSpec(), constraints = "LongOnly")
 portfolio_menor_risco
 
-# Elaborando e plotando a fronteira de markowitz dos ativos
+# Construindo a fronteira de eficiência
 fronteira <- portfolioFrontier(retornos)
-frontierPlot(fronteira, col = c("blue", "red"), pch = 20)
+
+# Plot da fronteira de eficiência
+frontierPlot(fronteira, col = c("blue", "red"), pch = 20, main = "Fronteira de Eficiência",
+             xlab = "Risco", ylab = "Retorno")
+
+# Adicionando alguns pontos na fronteira utilizando o método de Monte Carlo
 monteCarloPoints(fronteira,
-                 mcSteps = 5000,
+                 mcSteps = 50,
                  pch = 20,
                  cex = 0.25)
